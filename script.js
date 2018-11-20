ff68b416521d0c9908f592464b2b2b8b019c700a
Oleh1033 <oleg103356@gmail.com>
2018-11-20 13:39:17 +0100

    task3 1

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
 
     var currentPage = 1;
     var u = $("#users");
@@ -10,30 +9,33 @@ $(document).ready(function() {
 
     var showTable = function (page) {
         $.get(
-            "https://reqres.in/api/users?page="+page+"" ,
-            function(data) {
+            "https://reqres.in/api/users?page=" + page + "",
+            function (data) {
                 for (var i = 0; i < data.data.length; i++) {
-                    $('#users').append('<tr><td><a href="javascript: onClick=showPage('+data.data[i].id+')">edit</a></td><td>' + data.data[i].id + '</td><td>' + page + data.data[i].first_name +
+                    $('#users').append('<tr><td><a href="javascript: onClick=showPage(' + data.data[i].id + ')" >edit</a></td><td>' + data.data[i].id + '</td><td>' + page + data.data[i].first_name +
                         '</td><td>' + data.data[i].last_name + '</td><td><img src="' + data.data[i].avatar + '" alt=""></td><tr>');
                 }
             });
     };
 
+
+    // , data.data[i].first_name, data.data[i].last_name, data.data[i].avatar
+
     $.get(
-        "https://reqres.in/api/users?page=1" ,
-        function(data) {
+        "https://reqres.in/api/users?page=1",
+        function (data) {
 
             showTable(1);
 
             var totalPages = data.total_pages;
 
-            for (var j = 1; j < totalPages+1; j++) {
+            for (var j = 1; j < totalPages + 1; j++) {
                 $('#pagination').append('<li class="page-item"><a class="page-link" href="javascript:void(0)" >' + j + '</a></li>');
             }
 
             $("li.page-item").on("click", function () {
                 var current;
-                current = $(this).index()-1;
+                current = $(this).index() - 1;
                 currentPage = current;
                 cleanAppend();
                 showTable(current);
@@ -41,7 +43,7 @@ $(document).ready(function() {
             });
 
             $("#next").on('click', function () {
-                if(currentPage<totalPages){
+                if (currentPage < totalPages) {
                     cleanAppend();
                     currentPage++;
                     showTable(currentPage);
@@ -49,7 +51,7 @@ $(document).ready(function() {
             });
 
             $("#prev").on('click', function () {
-                if(currentPage>1){
+                if (currentPage > 1) {
                     cleanAppend();
                     currentPage--;
                     showTable(currentPage);
@@ -59,15 +61,29 @@ $(document).ready(function() {
 });
 
 
-var showPage = function (numCustomer) {
-    document.querySelector('.bg-modal').style.display = 'flex';
-//    document.querySelector('.bg-modal').style.display = 'none';
+var showPage = function (id) {
+
+    $.get(
+        "https://reqres.in/api/users/"+ id +"",
+        function (data) {
+            var id = data.data.id;
+            var firstName = data.data.first_name;
+            var lastName = data.data.last_name;
+            var avatar = data.data.avatar;
+
+            $('[name="id"]')[0].placeholder='id: '+id;
+            $('[name="firstName"]')[0].placeholder='First Name: '+firstName;
+            $('[name="lastName"]')[0].placeholder='Last Name: '+lastName;
+            $('[name="avatar"]')[0].placeholder='url foto: '+avatar;
+
+            $(".bg-modal").show();
+        })
 };
 
-document.querySelector('.close').addEventListener('click', function () {
-    document.querySelector('.bg-modal').style.display = 'none';
-});
 
+$('.close').click(function(){
+    $(".bg-modal").hide();
+});
 
 
 
