d5042939716c211eaeaba0fea5db3399defd2cc5
Oleh1033 <oleg103356@gmail.com>
2018-11-20 18:09:27 +0100

    task3 2

--- script.js
+++ script.js
@@ -12,14 +12,29 @@ $(document).ready(function () {
             "https://reqres.in/api/users?page=" + page + "",
             function (data) {
                 for (var i = 0; i < data.data.length; i++) {
-                    $('#users').append('<tr><td><a href="javascript: onClick=showPage(' + data.data[i].id + ')" >edit</a></td><td>' + data.data[i].id + '</td><td>' + page + data.data[i].first_name +
+                    $('#users').append('<tr><td><a href="javascript: onClick=showPage(' + data.data[i].id + ')" >edit</a></td><td>' + data.data[i].id + '</td><td>' + data.data[i].first_name +
                         '</td><td>' + data.data[i].last_name + '</td><td><img src="' + data.data[i].avatar + '" alt=""></td><tr>');
                 }
             });
     };
 
+    var chekNumber = function () {
 
-    // , data.data[i].first_name, data.data[i].last_name, data.data[i].avatar
+        var activeButton  = $('.active').attr("id");
+        var m =  +activeButton;
+        console.log(m);
+        if(m>3){
+            console.log('valva');
+            $("a#next").addClass(" disabled");
+        }else $("a#next").removeClass(" disabled");
+
+        if(m<2){
+            console.log('valva');
+            $("a#prev").addClass(" disabled");
+        }else $("a#prev").removeClass(" disabled");
+
+    };
+
 
     $.get(
         "https://reqres.in/api/users?page=1",
@@ -28,10 +43,14 @@ $(document).ready(function () {
             showTable(1);
 
             var totalPages = data.total_pages;
+            var prev;
 
             for (var j = 1; j < totalPages + 1; j++) {
-                $('#pagination').append('<li class="page-item"><a class="page-link" href="javascript:void(0)" >' + j + '</a></li>');
+                $('#pagination').append('<li class="page-item" id="'+ j +'"><a class="page-link" href="javascript:void(0)" >' + j + '</a></li>');
             }
+            $("li#1").addClass(" active");
+            
+            chekNumber();
 
             $("li.page-item").on("click", function () {
                 var current;
@@ -39,30 +58,43 @@ $(document).ready(function () {
                 currentPage = current;
                 cleanAppend();
                 showTable(current);
-
+                $('.active').removeClass('active');
+                $(this).addClass(" active");
+                chekNumber()
             });
 
             $("#next").on('click', function () {
+                if(currentPage==4)return;
                 if (currentPage < totalPages) {
                     cleanAppend();
                     currentPage++;
                     showTable(currentPage);
                 }
+                prev = $('.active').attr("id");
+                var n =  +prev + 1;
+                $('.active').removeClass('active');
+                $('#'+ n +'').addClass(" active");
+                chekNumber()
             });
 
             $("#prev").on('click', function () {
+                if(currentPage==1)return;
                 if (currentPage > 1) {
                     cleanAppend();
                     currentPage--;
                     showTable(currentPage);
                 }
+                prev = $('.active').attr("id");
+                var n = +prev - 1;
+                $('.active').removeClass('active');
+                $('#'+ n +'').addClass(" active");
+                chekNumber()
             });
         })
 });
 
 
 var showPage = function (id) {
-
     $.get(
         "https://reqres.in/api/users/"+ id +"",
         function (data) {
@@ -71,19 +103,30 @@ var showPage = function (id) {
             var lastName = data.data.last_name;
             var avatar = data.data.avatar;
 
-            $('[name="id"]')[0].placeholder='id: '+id;
-            $('[name="firstName"]')[0].placeholder='First Name: '+firstName;
-            $('[name="lastName"]')[0].placeholder='Last Name: '+lastName;
-            $('[name="avatar"]')[0].placeholder='url foto: '+avatar;
+            $('[name="id"]')[0].value=id;
+            $('[name="firstName"]')[0].value=firstName;
+            $('[name="lastName"]')[0].value=lastName;
+            $('[name="avatar"]')[0].value=avatar;
 
             $(".bg-modal").show();
+
+            $('.submit').click(function(){
+                $.post('https://reqres.in/api/users/2',
+                    {"data":{
+                        "id":""+ $('[name="id"]')[0].value +"",
+                        "first_name":"" + $('[name="firstName"]')[0].value + "",
+                        "last_name":""+$('[name="lastName"]')[0].value+"",
+                        "avatar":""+$('[name="avatar"]')[0].value+""}}
+                );
+            });
         })
 };
-
 
 $('.close').click(function(){
     $(".bg-modal").hide();
 });
+
+
 
 
 
